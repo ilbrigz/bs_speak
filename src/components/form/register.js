2b9fdc7df8a601e315e379705d838183a123f18f
ilbrigz <ilogirb23@yahoo.com>
2019-10-16 22:44:36 +0800

    changed db url

--- src/components/form/register.js
+++ src/components/form/register.js
@@ -2,7 +2,7 @@ const io = require("socket.io-client")
 const feathers = require("@feathersjs/feathers")
 const socketio = require("@feathersjs/socketio-client")
 
-const socket = io("http://localhost:3030/")
+const socket = io("https://backend-ixbudpbmkp.now.sh/")
 const client = feathers()
 
 client.configure(socketio(socket))
